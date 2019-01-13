4b526e34c20f69ad778f31fd3a7695bdb2fa75b9
Dray <hnzksq@gmail.com>
2019-01-14 01:12:18 +0800

    添加删推脚本

--- tweets_del.py
+++ tweets_del.py
@@ -6,16 +6,22 @@ import concurrent.futures
 from twitter import TwitterError
 from api import api, confirm
 
+id_list = [
+    "1001130169036259328",
+]
+
 # 搜索删除
-list = api.GetSearch(term = "#taichibot", since_id = 0)
-for tweet in list:
-    print(tweet)
-    api.DestroyStatus(tweet.id)
+# list = api.GetSearch(term="#taichibot", since_id = 0)
+# for tweet in list:
+#     id_list.append(tweet.id)
+#     # print(tweet)
 
 # 直接根据 id 删除
-id_list = [
-    "707446814979584004",
-]
 for ids in id_list:
-    res = api.DestroyStatus(ids)
-    print(ids, res)
+    try:
+        # res = api.GetStatus(ids)
+        # print(res.id, res.created_at, res.text, res.media)
+        res = api.DestroyStatus(ids)
+        print(ids, res)
+    except Exception as ex:
+        print(ids, ex)
